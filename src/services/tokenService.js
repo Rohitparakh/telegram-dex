1e6d27c09e97402dde3f940693b6e6f356c80546
Rohitparakh <rohitparakh4@gmail.com>
2024-10-14 23:05:00 +0530

    message formatting

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -110,7 +110,7 @@ ${user.isAdmin?`Total Boost: ${token.totalAmount} \n First Fetched At: ${new Dat
 5M: ${formatNumber(tokenDetails.volume.m5)} | 1H: ${formatNumber(tokenDetails.volume.h1)} | 6H: ${formatNumber(tokenDetails.volume.h6)} | 24H: ${formatNumber(tokenDetails.volume.h24)}
 🔑Liquidity: ${formatNumber(tokenDetails.liquidity.usd)} 
 
-${socialLinks?`🔐Socials: \n${socialLinks}\n \n`:``}${websiteLinks?`🔐Socials: \n${websiteLinks}\n \n`:``}[Dexscreener URL](${token.url})
+${socialLinks?`🔐Socials: \n${socialLinks}\n \n`:``}${websiteLinks?`🔐Socials: \n${websiteLinks}\n \n`:``}🔗 [Dexscreener URL](${token.url})
         `;
 //         *New boost found for ${tokenDetails.baseToken.name} (${tokenDetails.baseToken.symbol}):*
 //             Token Address: ${token.tokenAddress}
